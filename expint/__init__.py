5a36773b7126e7539bd8870125792e769f6df6ea
Robert Gantner <rngantner@gmail.com>
2012-04-30 15:36:32 +0200

    short docstrings in __init__.py files

---
+++ expint/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+"""expint - tools for exponential integrators
+This module contains various tools that are useful for exponential integrators,
+as well as implementations of some exponential and ordinary integrators.
+"""
+
+
---
+++ expint/problems/__init__.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+
+"""This module provides various ODEs formulated as subclasses of the RHS class.
+They are used primarily for testing the various integrators.
+"""
+
