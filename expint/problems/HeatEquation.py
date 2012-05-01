df914afa0facff9f96c245f0a76328599c0918f7
Robert Gantner <rngantner@gmail.com>
2012-05-01 15:57:20 +0200

    problem updates

--- expint/problems/HeatEquation.py
+++ expint/problems/HeatEquation.py
@@ -5,9 +5,29 @@ import scipy.sparse as sp
 import scipy.sparse.linalg as sl
 import os
 
-solutionFile = 'HeatEquation_solution.npz'
+#solutionFile = 'HeatEquation_solution.npz'
 #solutionFile = 'HeatEquation_solution2.npz'
 #solutionFile = 'HeatEquation_solution3.npz'
+HEsol = array([ 0.00127801,  0.00255598,  0.00383382,  0.00511139,  0.00638846,
+        0.00766478,  0.00894001,  0.01021376,  0.01148558,  0.01275499,
+        0.01402141,  0.01528423,  0.0165428 ,  0.01779639,  0.01904423,
+        0.02028551,  0.02151936,  0.02274488,  0.0239611 ,  0.02516703,
+        0.02636163,  0.02754382,  0.0287125 ,  0.02986653,  0.03100471,
+        0.03212585,  0.03322872,  0.03431206,  0.0353746 ,  0.03641503,
+        0.03743204,  0.03842432,  0.03939051,  0.04032929,  0.0412393 ,
+        0.04211919,  0.04296762,  0.04378323,  0.0445647 ,  0.04531068,
+        0.04601988,  0.04669098,  0.04732272,  0.04791383,  0.04846309,
+        0.04896929,  0.04943126,  0.04984786,  0.050218  ,  0.05054061,
+        0.05081468,  0.05103923,  0.05121335,  0.05133617,  0.05140687,
+        0.05142469,  0.05138894,  0.05129897,  0.05115421,  0.05095417,
+        0.0506984 ,  0.05038653,  0.05001829,  0.04959344,  0.04911187,
+        0.04857349,  0.04797835,  0.04732655,  0.04661827,  0.0458538 ,
+        0.04503349,  0.0441578 ,  0.04322727,  0.04224253,  0.04120431,
+        0.04011343,  0.03897078,  0.03777737,  0.0365343 ,  0.03524275,
+        0.033904  ,  0.03251944,  0.03109053,  0.02961883,  0.02810599,
+        0.02655377,  0.02496399,  0.0233386 ,  0.0216796 ,  0.0199891 ,
+        0.01826929,  0.01652245,  0.01475093,  0.01295719,  0.01114374,
+        0.00931318,  0.0074682 ,  0.00561153,  0.00374601,  0.00187451])
 
 class HeatEquation(RHS):
     """
@@ -52,13 +72,14 @@ class HeatEquation(RHS):
         
         # store standard initial values
         self.init1 = self.xvals*sin(pi*self.xvals)
+        self.sol1 = HEsol
         # solution to this initial value for t0=0, tend=1
-        if os.path.exists(solutionFile):
-            file = open(solutionFile)
-            self.sol1 = load(file)['y']
-            file.close()
-        else:
-            print "precomputed solution could not be imported"
+        #if os.path.exists(solutionFile):
+        #    file = open(solutionFile)
+        #    self.sol1 = load(file)['y']
+        #    file.close()
+        #else:
+        #    print "precomputed solution could not be imported"
     def normDf(self,x):
         return 6*self.N**(2.5)*norm(x[:-1])
     def ApplyDf(self,u,v):
--- expint/problems/LotkaVolterra.py
+++ expint/problems/LotkaVolterra.py
@@ -1,3 +1,4 @@
+import numpy as np
 from expint.RHS import DfRHS
 
 class LotkaVolterra(DfRHS):
